21a13292f5cbcdf54b5e302427ed0d5dcc3b9798
nathanieltetteh <mr.nathanieltetteh@gmail.com>
2023-02-21 01:23:47 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
